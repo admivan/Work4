2a0ddf313fb0a422cdfd5de8e689c86e6f19209e
admivan <popded51@gmail.com>
2021-12-17 14:44:44 +0300

    Перекинуть домой

--- src/main/java/ru/dolgov/Work3.java
+++ src/main/java/ru/dolgov/Work3.java
@@ -5,67 +5,77 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Work3 {
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = '0';
+    public static char[][] map;
+    public static int SIZE = 3;
+    public static final char DOT_EMRTY = '-';
     public static Scanner scanner = new Scanner(System.in);
-    public static Random random= new Random();
-    public static int SIZE=3;
-    public static int DOTS_TO_WIN=3;
-    public static final char DOT_EMPTY = '-';
-    public static final char DOT_X='X';
-    public static final char DOT_O='0';
-    public static char[][] map= new char[SIZE][SIZE];
+    public static Random random = new Random();
+
     public static void main(String[] args) {
         initMap();
         printMap();
-        humanTurn();
-        pcTurman();
+        humanPlayer();
+        printMap();
+        aiPlayer();
         printMap();
     }
 
-    public static void humanTurn() {
+    public static void humanPlayer () {
         int x;
         int y;
+
         do {
-            System.out.println("Введите координаты X,Y для хода");
-                x=scanner.nextInt()-1;
-                y=scanner.nextInt()-1;
-        }while (isCellValid(x, y));
-        map[y][x]=DOT_X;
+            x=scanner.nextInt()-1;
+            y=scanner.nextInt()-1;
+        }while (!isCellValid(x,y));
+        map[x][y]=DOT_X;
     }
 
-    public static void pcTurman(){
+    private static boolean isCellValid(int x, int y) {
+        if (x<0||x>=SIZE||y<0||y>=SIZE){
+            return false;
+        }
+        if(map[x][y]==DOT_EMRTY){
+            return true;
+        }
+        return false;
+    }
+
+    public static void aiPlayer () {
         int x;
         int y;
         do {
             x= random.nextInt(SIZE);
-            y=random.nextInt(SIZE);
-        }while (isCellValid(x, y));
-        System.out.println("Компьютер сходил"+(x+1)+" "+(y+1));
-        map[y][x]=DOT_O;
-    }
-
-    public static boolean isCellValid(int x, int y){
-        if (x<0||x>=SIZE||y<0||y>=SIZE){
-            return false;
-        }else return map[y][x] != DOT_EMPTY;
+            y= random.nextInt(SIZE);
+        }while (!isCellValid(x,y));
+        map[x][y]=DOT_O;
     }
 
     private static void printMap() {
-        for (int i=0; i<=SIZE;i++){
+        System.out.print("  ");
+        for (int i = 1; i<=SIZE; i++){
+
             System.out.print(i+" ");
         }
         System.out.println();
-        for (int i=0;i<SIZE;i++){
+        for (int i = 0; i < SIZE; i++) {
             System.out.print((i+1)+" ");
-            for (int j=0;j<SIZE;j++){
+            for (int j = 0; j < SIZE; j++) {
                 System.out.print(map[i][j]+" ");
             }
             System.out.println();
         }
     }
 
+
     private static void initMap() {
-        for (char[] chars : map) {
-            Arrays.fill(chars, DOT_EMPTY);
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMRTY;
+            }
         }
     }
 }
